1629a8d8260bf76f119e0f6c4f713c545d0b1e8b
brennoviana <brennovianacoelho@gmail.com>
2023-04-07 20:29:35 -0300

    terceiro commit

---
+++ ecommerce_app/static/js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.image-main').slick({
+      dots: true,
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 2500, // tempo em milissegundos entre as transições
+      infinite: true
+    });
+  });
