458a8e9c7ea5b01393572c282db3fd337a43296e
brennoviana <brennovianacoelho@gmail.com>
2023-04-13 20:06:32 -0300

    brenno commit 3

--- ecommerce_app/models.py
+++ ecommerce_app/models.py
@@ -6,4 +6,4 @@ class produto(models.Model):
     nome_prod = models.CharField(max_length=50)
     texto_prod = models.TextField(max_length=200)
     preco_prod = models.FloatField()
-    img_prod = models.ImageField(upload_to='media/prod/%Y/%m/%d/', default='produtos/sem_imagem.jpg')
+    img_prod = models.ImageField(upload_to='prod/%Y/%m/%d/', default='produtos/sem_imagem.jpg')
