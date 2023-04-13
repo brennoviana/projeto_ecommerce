5aefcc85c3ce9466d4761bd64fe9d3fd3b4a5eba
brennoviana <brennovianacoelho@gmail.com>
2023-04-13 19:06:30 -0300

    brenno commit 2

--- ecommerce_app/views.py
+++ ecommerce_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
 
 def index(request):
-    return render(request, 'pages/index.html')
+    prods = models.produto.objects.all()
+    prods_dic = {"prods" : prods}
+    return render(request, 'pages/index.html', prods_dic)
