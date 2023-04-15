5c7372ddd43bc375af0c61f4e0302e5eac04fbea
brennoviana <brennovianacoelho@gmail.com>
2023-04-15 17:33:12 -0300

    brenno commit nao lembro 2

--- ecommerce_app/urls.py
+++ ecommerce_app/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.Index.as_view(), name='index'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.Logout.as_view(), name='logout')
 ]
--- ecommerce_app/views.py
+++ ecommerce_app/views.py
@@ -1,14 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from . import forms
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login
 
-def index(request):
+from django.views import View
 
-    prods = models.produto.objects.all()
-    prods_dic = {
-        "prods" : prods,
-        "home" : "Home"
-        }
+class Index(View):
+    template_name = 'pages/index.html'
+    def get(self, request):
+
+        prods = models.produto.objects.all()
+        prods_dic = {
+            "prods" : prods,
+            "home" : "Home"
+            }
+
+        return render(request, self.template_name, prods_dic)
+
+class Login(View):
+    template_name = 'pages/login.html'
+    form = forms.LoginForm()
+    context = {"form" : form}
+
+    def get(self, request):
+        return render(request, self.template_name, self.context)
     
-    return render(request, 'pages/index.html', prods_dic)
+    def post(self, request):
+        form = forms.LoginForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
 
+class Logout(View):
+    def get(self, request):
+        logout(request)
+        return redirect("index")
