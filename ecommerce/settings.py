fe4a564b9e6b9a9731f72a2ad1a9846b4ca7a24e
brennoviana <brennovianacoelho@gmail.com>
2023-04-16 12:39:08 -0300

    brenno commit nao lembro 5

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap4',
     'ecommerce_app',
-
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- ecommerce_app/models.py
+++ ecommerce_app/models.py
@@ -16,12 +16,10 @@ class prod(models.Model):
     name_prod = models.CharField(max_length=50)
     text_prod = models.TextField(max_length=200)
     price_prod = models.FloatField()
-    img_prod = models.ImageField(upload_to='prod/%Y/%m/%d/', default='produtos/sem_imagem.jpg')
+    img_prod = models.ImageField(upload_to='prod/%Y/%m/%d/', default='')
     category = models.CharField(max_length=12, choices=CATEGORIAS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
-
-
     def __str__(self):
         return f"{self.id} - {self.name_prod}"
